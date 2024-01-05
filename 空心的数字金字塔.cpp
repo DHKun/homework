c2b7cc222266eb60abb95f45bad7f70dad2e5367
DoHoKun <2727461997@qq.com>
2024-01-05 13:07:36 +0800

    update

--- 空心的数字金字塔.cpp
+++ 空心的数字金字塔.cpp
@@ -12,33 +12,49 @@ int main()
     return 0;
 }
 
-void hollowPyramid ( int n )
+// void hollowPyramid ( int n )
+// {
+//     int i,j,k,z;
+//     for(i=1;i<=n;i++)
+//     {
+//         for(j=n-i;j>0;j--)
+//         {
+//             printf(" ");
+//         }
+//         printf("%d",i);
+//         if(i==1) printf("\n");
+//         if(i>=2&&i<n)
+//         {
+//             for(k=0;k<2*i-3;k++)
+//             {
+//                 printf(" ");
+//             }
+//             printf("%d\n",i);
+//         }
+//     }
+//     i=i-1;
+//     if(i==n)
+//     {
+//         for(z=0;z<2*n-2;z++)
+//         {
+//             printf("%d",i);
+//         }
+//     }
+//     return;
+// }
+void hollowPyramid( int n )
 {
-    int i,j,k,z;
-    for(i=1;i<=n;i++)
-    {
-        for(j=n-i;j>0;j--)
-        {
+	int i,j;
+	for(i=1;i<=n;i++){
+		for(j=0;j<n-i;j++){
             printf(" ");
         }
-        printf("%d",i);
-        if(i==1) printf("\n");
-        if(i>=2&&i<n)
-        {
-            for(k=0;k<2*i-3;k++)
-            {
-                printf(" ");
-            }
-            printf("%d\n",i);
-        }
-    }
-    i=i-1;
-    if(i==n)
-    {
-        for(z=0;z<2*n-2;z++)
-        {
-            printf("%d",i);
-        }
-    }
-    return;
+		for(j=0;j<2*i-1;j++){
+			if(j==0||n==i||j==2*i-2)
+			printf("%d",i);
+			else
+			printf(" ");
+		} 
+		printf("\n");
+	}
 }
